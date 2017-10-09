aa201af9d6e6a9b93ca6f39c85a5bebbb451b6af
Pavlo Yerzhykevych <py@interlogic.com.ua>
2017-10-09 10:25:27 +0300

    Remove request models

--- AVS.NET/Auth/Tokens/CodeGrandToken.cs
+++ AVS.NET/Auth/Tokens/CodeGrandToken.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 
 namespace AVS.Auth.Tokens
 {
--- AVS.NET/Auth/Tokens/ImplicitGrantToken.cs
+++ AVS.NET/Auth/Tokens/ImplicitGrantToken.cs
@@ -1,17 +1,6 @@
-﻿using AVS.Auth.Requests;
-
-namespace AVS.Auth.Tokens
+﻿namespace AVS.Auth.Tokens
 {
     public class ImplicitGrantToken : AuthToken
     {
-        public static implicit operator ImplicitGrantToken(ImplicitGrantRequest request)
-        {
-            return new ImplicitGrantToken
-            {
-                AccessToken = request.AccessToken,
-                TokenType = request.TokenType,
-                ExpiresIn = request.ExpiresIn
-            };
-        }
     }
 }
